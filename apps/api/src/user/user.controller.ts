096c78049b7253f08b0914b8fe97949ccc6c5b59
Adam Rafiandri <rafiandria23@gmail.com>
2024-02-11 22:32:42 +0700

    feat(api/user): implement user controller

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+import { User } from '../app/decorators/auth.decorator';
+import { AppService } from '../app/app.service';
+
+@Controller('/api/v1/users')
+export class UserController {
+  constructor(private readonly appService: AppService) {}
+
+  @User()
+  @Get('/')
+  @HttpCode(HttpStatus.OK)
+  public helloWorld() {
+    return this.appService.successTimestamp({
+      data: {
+        message: 'Hello, world!',
+      },
+    });
+  }
+}
