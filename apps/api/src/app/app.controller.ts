fc21318ef2e688592a081815486a4cc6e546f38b
Adam Rafiandri <rafiandria23@gmail.com>
2024-02-11 22:34:13 +0700

    feat(api/app): implement app controller

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 
+import { Public } from './decorators/auth.decorator';
 import { AppService } from './app.service';
 
 @Controller('/api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get('/')
+  @HttpCode(HttpStatus.OK)
   public helloWorld() {
     return this.appService.successTimestamp({
       data: {
