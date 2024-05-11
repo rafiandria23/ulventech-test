a53d96b7346411d81d8deb8d4875cf50b0c436df
Adam Rafiandri <rafiandria23@gmail.com>
2024-05-11 12:32:27 +0700

    chore(api/env): revise DB configs

--- apps/api/src/app/configs/jwt.config.ts
+++ apps/api/src/app/configs/jwt.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => {
   return {
-    admin_secret: _.defaultTo(process.env.JWT_ADMIN_SECRET, 'ulventech'),
-    user_secret: _.defaultTo(process.env.JWT_USER_SECRET, 'ulventech'),
+    admin_secret: _.defaultTo(process.env.JWT_ADMIN_SECRET, 'ulventech_admin'),
+    user_secret: _.defaultTo(process.env.JWT_USER_SECRET, 'ulventech_user'),
   };
 });
